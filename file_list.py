db464cb10f1ecef2543cc9f2648a112974563d81
Dipankar Pal <dipankarpal5050@gmail.com>
2020-09-04 20:27:38 +0530

    Updated: files

--- file_list.py
+++ file_list.py
@@ -7,8 +7,9 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 content = ""
 i = 1
 for file in onlyfiles:
-    add = f"{str(i)}. [{file.split('.')[0]}](./stories/{file})"
+    add = f"{str(i)}. [{file.split('.')[0].replace('_',' ')}](./stories/{file})"
     print(add)
+    print("")
     # content = content + add
     i = i+ 1
     content = content+"\n"
